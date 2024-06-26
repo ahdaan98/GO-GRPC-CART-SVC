e77cbcb86f3a81c5d824fe4e14ec6c505de09edc
Ahdaan Hashim <ahdaanhashim67@gmail.com>
2024-06-26 23:50:25 +0530

    cart service

---
+++ cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+
+	"cart/service/pkg/config"
+	"cart/service/pkg/di"
+)
+
+func main() {
+
+	config, configErr := config.LoadConfig()
+	if configErr != nil {
+		log.Fatal("cannot load config: ", configErr)
+	}
+
+	server, diErr := di.InitializeAPI(config)
+
+	if diErr != nil {
+		log.Fatal("cannot start server: ", diErr)
+	} else {
+		server.Start()
+	}
+
+}
